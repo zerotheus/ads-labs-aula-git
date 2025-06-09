1dd575aa637299d97c5deb32f2340e9906a3a8f9
matheus <matheusoliveira.7313@gmail.com>
2025-06-09 16:53:41 -0300

    subtrair

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ function somar(a, b) {
 	return a + b
 }
 
+function subtrair(a, b) {
+	return a - b
+}
+
 function multiplicar(a, b) {
 	return a * b
 }
 
-function dividir(a, b){
+function dividir(a, b) {
 	return a / b
 }
